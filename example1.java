48b21d1e57d24d222056211fe4c176cf0b63029f
Satyam7798 <105342385+Satyam7798@users.noreply.github.com>
2022-05-12 10:48:22 +0530

    I AM SATYAM CHANGE SORCE CODE

--- example1.java
+++ example1.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class example1 {
 	public static void main(String args[])
 	{	  
-		  System.out.println("Hello Learner");  
+		  System.out.println("HelL Learner");  
 		 System.out.println("Hello JFS developer");
 	}
 }
